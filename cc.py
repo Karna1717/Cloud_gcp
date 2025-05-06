c17945974f82db3065de9bd70285db373eee33fa
Karna1717 <karan.tathe777@gmail.com>
2025-05-07 01:05:07 +0530

    Update cc.py

--- cc.py
+++ cc.py
@@ -1,1 +1,6 @@
-print("hello karan")
+a = int(input("Enter First Number: "))
+b = int(input("Enter Second Number: "))
+
+print("First Number is:", a)
+print("Second Number is:", b)
+print("Addition is:", a + b)
